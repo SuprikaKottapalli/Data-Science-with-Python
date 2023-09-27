c141f4b3f334c8f4fd7d0599a5898f782ce966de
Suprika Kottapalli <127580112+SuprikaKottapalli@users.noreply.github.com>
2023-09-27 10:04:47 +0530

    Add files via upload

---
+++ Replication.py
@@ -0,0 +1,6 @@
+#String replication
+s = " * "
+S1 = s*5
+print(S1)
+
+print(" Suprika "*10)
---
+++ Slicing.py
@@ -0,0 +1,8 @@
+#String Slicing
+#string[start:end:step]
+
+
+a ="Hello World"
+print(a[4:])
+print(a[1:4])
+print(a[6::2])
